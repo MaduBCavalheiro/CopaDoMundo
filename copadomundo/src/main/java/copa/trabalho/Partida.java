43f1c3547dcd1a14f1688ec382aea3c7453ba816
CauanMendes05 <cauanmendeslopes@gmail.com>
2026-05-29 15:11:51 -0300

    ajuste partida

--- copadomundo/src/main/java/copa/trabalho/Partida.java
+++ copadomundo/src/main/java/copa/trabalho/Partida.java
@@ -1,6 +1,7 @@
 package copa.trabalho;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
 public class Partida {
@@ -13,6 +14,7 @@ public class Partida {
 
     private double duracao;
     private LocalDateTime data;
+    private DateTimeFormatter formatador;
 
     private int cartoesAmarelos;
     private int cartoesVermelhos;
@@ -20,10 +22,12 @@ public class Partida {
     private boolean encerrada;
     private Time vencedor;
 
+    private Estadio estadio;
+
     // Evento especial da partida
     private Evento eventoPartida;
 
-    public Partida(Time t1, Time t2) {
+    public Partida(Time t1, Time t2, Estadio estadio) {
         this.t1 = t1;
         this.t2 = t2;
 
@@ -32,6 +36,7 @@ public class Partida {
 
         this.duracao = 90.0;
         this.data = LocalDateTime.now();
+        this.formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 
         this.cartoesAmarelos = 0;
         this.cartoesVermelhos = 0;
@@ -40,6 +45,8 @@ public class Partida {
         this.vencedor = null;
 
         this.eventoPartida = null;
+
+        this.estadio = estadio;
     }
 
     // GETTERS
@@ -83,15 +90,31 @@ public class Partida {
         }
         else {
             // Mata-mata não permite empate
-            System.out.println("\n⚽ Empate no tempo normal!");
-            System.out.println("⚽ Decisão nos pênaltis...\n");
-
+            int min = 5;
+            int max = 10;
             if (random.nextBoolean()) {
                 vencedor = t1;
+                vencedor.AdicionarPontos(random.nextInt((max-min) + 1) + min);
+                t2.AdicionarPontos(random.nextInt(min+1));
             }
             else {
                 vencedor = t2;
+                vencedor.AdicionarPontos(random.nextInt((max-min) + 1) + min);
+                t1.AdicionarPontos(random.nextInt(min+1));
             }
+
+            System.out.println("\n# Empate no tempo normal!");
+            System.out.println("# Decisão nos pênaltis...\n");
+
+            System.out.println("Devido ao empate, a quantidade acumulada de pontos de cada time foi comparada.");
+            System.out.println(t1.getNome() + ": " + t1.getPontos() + " pontos.");
+            System.out.println(t2.getNome() + ": " + t2.getPontos() + " pontos.");
+            System.out.println("Por possuir uma quantidade maior de pontos, " + vencedor.getNome() + " passa adiante.");
+
+            System.out.println(
+                "Vencedor nos pênaltis: " +
+                vencedor.getNome()
+            );
         }
 
         encerrada = true;
@@ -139,7 +162,7 @@ public class Partida {
     
     public void relatorio() {
         System.out.println("\n══════════════════════════════════");
-        System.out.println("      ⚽ RELATÓRIO DA PARTIDA ⚽");
+        System.out.println("      # RELATÓRIO DA PARTIDA #");
         System.out.println("══════════════════════════════════");
 
         System.out.println(
@@ -155,6 +178,12 @@ public class Partida {
 
         if (golsT1 == golsT2) {
             System.out.println("\nEmpate no tempo normal.");
+
+            System.out.println("Devido ao empate, a quantidade acumulada de pontos de cada time foi comparada.");
+            System.out.println(t1.getNome() + ": " + t1.getPontos() + " pontos.");
+            System.out.println(t2.getNome() + ": " + t2.getPontos() + " pontos.");
+            System.out.println("Por possuir uma quantidade maior de pontos, " + vencedor.getNome() + " passa adiante.");
+
             System.out.println(
                 "Vencedor nos pênaltis: " +
                 vencedor.getNome()
@@ -175,7 +204,7 @@ public class Partida {
 
         System.out.println(
             "Data: " +
-            data
+            data.format(formatador)
         );
 
         System.out.println(
@@ -188,6 +217,20 @@ public class Partida {
             cartoesVermelhos
         );
 
+        System.out.println(
+            "Técnico do "+ t1.getNome() +": " +
+            t1.getTecnico()
+        );
+
+        System.out.println(
+            "Técnico do "+ t2.getNome() +": " +
+            t2.getTecnico()
+        );
+
+        System.out.println(
+            "Estádio: "+ estadio.getNome() +" | " +
+            estadio.getPais()
+        );
       
         if (eventoPartida != null) {
             System.out.println(
@@ -195,11 +238,6 @@ public class Partida {
             );
             eventoPartida.exibirEvento();
         }
-
-        System.out.println(
-            "\nPartida encerrada: " +
-            encerrada
-        );
 
         System.out.println(
             "══════════════════════════════════\n"
